959a135a97826d4fde06c5b9a519d5320288d36c
Hauns <hauns1337@gmail.com>
2026-04-09 11:06:48 +0200

    unused

--- src/features/chatbot/api/chatService.js
+++
@@ -1,6 +0,0 @@
-import { axiosClient } from '../../../shared/api/axiosClient';
-
-export async function sendMessage(url, prompt) {
-  const { data } = await axiosClient.post(url, { prompt });
-  return data.answer ?? data.message ?? JSON.stringify(data, null, 2);
-}
